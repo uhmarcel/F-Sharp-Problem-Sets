0d5abb2bc77fb10cde2d8a612e01a402232812e4
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-10 12:30:49 -0500

    First F# application commit. Hello world application, asks for user name and returns a message.

---
+++ Hello world/Hello world/Program.fs
@@ -0,0 +1,16 @@
+﻿// First program in F#
+// Asks for your name and returns a message
+
+open System
+
+// Function to ask name and display msg
+let helloWorld() =  
+   
+    printf "Enter your name: "      // Retrieve user name
+    let name = Console.ReadLine()
+    
+    printfn "Hello %s from F#!" name    // Display message
+    Console.ReadKey() |> ignore         // Wait for key before exit
+
+helloWorld()
+
