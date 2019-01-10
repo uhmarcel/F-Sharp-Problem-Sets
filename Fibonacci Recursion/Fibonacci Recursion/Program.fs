c5c8b088ca1429ce298783b5ee12632dd7708842
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-10 16:20:14 -0500

    First Commit. Start Fibonacci sequence recursion program. Recursive function implemented, UI missing.

---
+++ Fibonacci Recursion/Fibonacci Recursion/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let rec fibonacci (n : int) : int =
+    if n = 0 then 0
+    elif n = 1 then 1
+    else fibonacci(n-1) + fibonacci(n-2) 
+
+[<EntryPoint>]
+let main argv =
+    printfn "Fibonacci Sequence"
+    printf "Enter kth number for fibonacci sequence: "
+
+    let k = Console.ReadLine |> int
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
