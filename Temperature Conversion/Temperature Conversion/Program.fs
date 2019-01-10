86b7e2f7b685660a9508af8e32d948cfa38d6035
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-10 13:12:37 -0500

    Program finished: Added conditionals for either temperature unit, and functions for both conversions.

--- Temperature Conversion/Temperature Conversion/Program.fs
+++ Temperature Conversion/Temperature Conversion/Program.fs
@@ -1,14 +1,29 @@
 ﻿open System
 
+let toCelsius(temp) = 
+    (temp - 32) * 5 / 9
+
+let toFahrenheit(temp) =
+    (temp * 5 / 9) + 32
+
+let INVALID_SELECTION = -1
+
 [<EntryPoint>]
 let main argv =
     printfn "Temperature Conversion Program"
-    printf "Select either Fahrenheit or Celsius (f/c): "
+    
+    printf "Enter a temperature to convert: "
+    let temperature = Console.ReadLine() |> int
+
+    printf "Enter the temperature unit ('f' for Fahrenheit / 'c' for Celsius): "
     let unit = Console.ReadLine()
 
-    if unit = "f"
-    then printfn "OK"
-    else printfn "NOT OK"
+    let output = 
+        if unit = "f" then toCelsius(temperature)  
+        elif unit = "c" then toFahrenheit(temperature)
+        else INVALID_SELECTION
+    
+    printfn "Converted temperature: %d" output
 
     Console.ReadKey() |> ignore
     0
