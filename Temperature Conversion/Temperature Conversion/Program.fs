b069dcb9a5220052a91a06362052d575b348dc68
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-10 12:45:35 -0500

    Started UI for the program, asking the user to select the temperature unit for the conversion program

---
+++ Temperature Conversion/Temperature Conversion/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Temperature Conversion Program"
+    printf "Select either Fahrenheit or Celsius (f/c): "
+    let unit = Console.ReadLine()
+
+    if unit = "f"
+    then printfn "OK"
+    else printfn "NOT OK"
+
+    Console.ReadKey() |> ignore
+    0
