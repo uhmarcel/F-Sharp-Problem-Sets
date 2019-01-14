7f497b6361993d06dc12fc95bee6179f2a0b017b
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-14 11:04:01 -0500

    First commit Started pogram to explore list operations Added two lists, and display their values to console.

---
+++ Exploring Lists/Exploring Lists/Program.fs
@@ -0,0 +1,14 @@
+﻿
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Exploring Lists in F#"
+
+    let A = [2;3;4]
+    let B = [5;7;8]
+
+    printfn "List A = %A" A
+    printfn "List B = %A" B
+
+    0 // return an integer exit code
