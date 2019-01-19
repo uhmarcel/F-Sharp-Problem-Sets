4d61110060d310c12c3bd6ee8930ef877056eb21
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-18 19:26:57 -0500

    First commit Created curried function to return a function to determine if the element is a multiple of a given input Started process up to solution: at the moment the program outputs a list of all multiples of 3 and 5 up to 1000

---
+++ ProjectEuler - Multiples of 3 and 5/ProjectEuler - Multiples of 3 and 5/Program.fs
@@ -0,0 +1,21 @@
+﻿(*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+*)    
+
+open System
+
+let isMultipleOfN n = (fun a -> a % n = 0)
+ 
+[<EntryPoint>]
+let main argv =
+    let naturalUptoThousand = [1..1000]
+
+    let multiplesThree = naturalUptoThousand |> List.filter (isMultipleOfN 3)
+    let multiplesFive = naturalUptoThousand |> List.filter (isMultipleOfN 5)
+
+    let mainList = multiplesThree @ multiplesFive
+    printfn "Current list:\n%A" mainList
+
+    Console.ReadKey() |> ignore
+    0
