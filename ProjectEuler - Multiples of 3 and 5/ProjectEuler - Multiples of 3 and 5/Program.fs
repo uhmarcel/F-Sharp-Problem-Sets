7c04d6660289f317c94bfb23cf3ee69a00ce77ca
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-18 19:44:04 -0500

    Problem solved Curried function removed, instead one boolean function is used as a filter to select elements which are multiples of 3 and 5. This solved the problem of having to concatenate later on, which created duplicates of the same numbers. List modified from [1..1000] to [1..999] as 1000 is not included. Operations compressed into one line using the pipe operator.

--- ProjectEuler - Multiples of 3 and 5/ProjectEuler - Multiples of 3 and 5/Program.fs
+++ ProjectEuler - Multiples of 3 and 5/ProjectEuler - Multiples of 3 and 5/Program.fs
@@ -1,24 +1,17 @@
-﻿(*
-    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-    Find the sum of all the multiples of 3 or 5 below 1000.
-*)    
+﻿open System
 
-open System
-
-let isMultipleOfN n = (fun a -> a % n = 0)
+let isMultThreeAndFive a = 
+    (a % 3 = 0) || (a % 5 = 0)
  
 [<EntryPoint>]
 let main argv =
-    let naturalUptoThousand = [1..1000]
-    let multiplesThree = naturalUptoThousand |> List.filter (isMultipleOfN 3)
-    let multiplesFive = naturalUptoThousand |> List.filter (isMultipleOfN 5)
-    let mainList = multiplesThree @ multiplesFive
-    let solution = mainList |> List.sum
-
     printfn "Project Euler - Multiples of 3 and 5 \n"
     printfn "If we list all the natural numbers below 10 that are multiples of 3 or 5,"
     printfn "we get 3, 5, 6 and 9. The sum of these multiples is 23."
     printfn "Find the sum of all the multiples of 3 or 5 below 1000. \n"
+
+    let solution = [1..999] |> List.filter isMultThreeAndFive |> List.sum
+
     printfn "Solution: %i" solution
 
     Console.ReadKey() |> ignore
