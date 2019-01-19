430b8bfcc801655e655538917c18c1cd18491299
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-18 20:04:33 -0500

    Problem solved Added sum of squares as a list piped to sum and then squared, then the square of sums mapping the elements to a square function, then summed.

---
+++ ProjectEuler - Sum square difference/ProjectEuler - Sum square difference/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Project Euler - Sum square difference \n"
+    printfn "Find the difference between the sum of the squares of the first"
+    printfn "one hundred natural numbers and the square of the sum. \n"
+
+    let sumOfSquares = [1I..100I] |> List.sum |> (fun a -> a * a) 
+    let squareOfSums = [1I..100I] |> List.map (fun a -> a * a) |> List.sum
+    let solution = sumOfSquares - squareOfSums
+
+    printfn "Solution: %A" solution
+
+    Console.ReadKey() |> ignore
+    0
