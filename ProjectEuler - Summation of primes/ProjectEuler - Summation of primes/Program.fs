c6e54010724632f9d7a4cba6cdceb7b4ab947c5c
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-23 14:12:22 -0500

    First commit Started problem 10 of project euler Created a isDivisible function to check if all numbers on a list are divisible by a value. Created a isPrime function using isDivisible functionality. Currently the solution is not working, all numbers are returning true as primes.

---
+++ ProjectEuler - Summation of primes/ProjectEuler - Summation of primes/Program.fs
@@ -0,0 +1,24 @@
+﻿// Project Euler - Problem 10
+// Summation of primes
+// Find the sum of all the primes below two million.
+
+open System
+
+let MAX = 2000000I
+
+let rec isDivisible list n =
+    match list with
+    | head :: tail -> head % n <> 0 & isDivisible tail n
+    | [] -> true
+
+let isPrime n = 
+    isDivisible [2..n-1] n
+
+
+[<EntryPoint>]
+let main argv =
+    let test = 6
+    printfn "Is number %i prime = %A" test (isPrime test) 
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
