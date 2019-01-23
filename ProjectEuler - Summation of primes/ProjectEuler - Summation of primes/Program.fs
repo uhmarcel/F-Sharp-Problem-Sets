7005b5cf4dc2f8a8e76b301caca7da3ed9a46684
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-23 15:17:02 -0500

    Attempt to implement Sieve of Eratosthenes First attempt to use the Sieve of Eratosthenes algorithm to reduce the time complexity of the solution. At the moment it is not working; the function seems to be returning another function instead of a list.

--- ProjectEuler - Summation of primes/ProjectEuler - Summation of primes/Program.fs
+++ ProjectEuler - Summation of primes/ProjectEuler - Summation of primes/Program.fs
@@ -6,16 +6,22 @@ open System
 
 let MAX = 10000
 
-let isPrime n = 
-    [2..n-1] |> List.forall (fun elem -> n % elem <> 0)
-
+let rec sieveOfErat (list: int list) (n: int) (limit: int) =
+    if n = limit then 
+        list
+    else
+        list |> List.filter (fun elem -> elem % n <> 0) |> sieveOfErat <|| (n+1, limit)
+    
+let primesUpTo n =
+    sieveOfErat [2..n] n
+    
 [<EntryPoint>]
 let main argv =
     printfn "ProjectEuler - Problem 10"
     printfn "Summation of primes"
     printfn "Find the sum of all the primes below two million \n"
 
-    let solution = [2..MAX] |> List.filter isPrime |> List.sum
+    let solution = primesUpTo MAX |> List.sum
 
     printfn "Sum of primes up to %i = %i" MAX solution
 
