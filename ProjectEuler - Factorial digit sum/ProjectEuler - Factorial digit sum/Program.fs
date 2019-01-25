537448cfe807dd98eacbf8fd6f9af8854c875864
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-24 22:59:13 -0500

    First commit Added a simple recursive function to compute the factorial of a number.

---
+++ ProjectEuler - Factorial digit sum/ProjectEuler - Factorial digit sum/Program.fs
@@ -0,0 +1,17 @@
+﻿// Project Euler - Problem 20
+// Factorial digit sum
+// Find the sum of the digits in the number 100!
+
+open System
+
+let rec factorial n =
+    match n with
+    | 0 | 1 -> 1
+    | _ -> n * factorial (n-1)
+
+[<EntryPoint>]
+let main argv =
+    let test = 5
+    printfn "Factorial(%i) = %i" test (factorial test)
+    Console.ReadKey() |> ignore
+    0
