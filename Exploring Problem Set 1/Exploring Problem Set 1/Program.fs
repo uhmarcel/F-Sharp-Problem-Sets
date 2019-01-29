5ef5a4c0edd74b0b6c0ff4b11f3f4348f1b47159
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-29 18:39:55 -0500

    First commit Added test cases to find how F# determines the order of execution of functions, related to problem 5 of problem set 1

---
+++ Exploring Problem Set 1/Exploring Problem Set 1/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    
+    let A = [[1;2;3];[55;2;7]]
+    let B = [8;9;10]
+
+    // Test for problem 5
+    let test = ((List.map List.head) A) @ B
+    printfn "Test: %A" test
+
+    Console.ReadKey() |> ignore
+    0
