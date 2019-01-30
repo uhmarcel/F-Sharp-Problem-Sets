c393be9f375b08e69f9cf7cf7f5aec50de19ceea
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-30 09:36:22 -0500

    More problems from set 1 Added problem 8 test Added a fibonacci function using a different syntax

--- Exploring Problem Set 1/Exploring Problem Set 1/Program.fs
+++ Exploring Problem Set 1/Exploring Problem Set 1/Program.fs
@@ -3,12 +3,33 @@
 [<EntryPoint>]
 let main argv =
     
-    let A = [[1;2;3];[55;2;7]]
-    let B = [8;9;10]
-
     // Test for problem 5
-    let test = ((List.map List.head) A) @ B
-    printfn "Test: %A" test
+    let A = [[1;2;3];[5;6;7]]
+    let B = [8;9;10]
+    
+    printfn "Problem 5:"
+    printfn "A = %A" A
+    printfn "B = %A" B
+    printfn  "((List.map List.head) A) @ B = %A\n" (((List.map List.head) A) @ B)
 
+    // Test for problem 8
+    let rec foo = function
+        | (xs, []) -> xs
+        | (xs, y::ys) -> foo (xs@[y], ys)
+    let A = [1;2;3]
+    let B = [7;8]
+
+    printfn "Problem 8:"
+    printfn "foo %A %A = %A\n" A B (foo (A,B))
+
+    // Test rec format
+    let rec fib = function
+        | n when n = 0 -> 1
+        | n when n = 1 -> 1
+        | n -> fib (n-1) + fib (n-2) 
+    let test = 4;
+
+    printfn "Testing rec formats"
+    printfn "Fib(%i) = %A" test (fib(test))
     Console.ReadKey() |> ignore
     0
