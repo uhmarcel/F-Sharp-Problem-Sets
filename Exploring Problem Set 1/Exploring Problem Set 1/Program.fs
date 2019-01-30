e4e1d1b496cb68089b9e3dd98d60cb290188b86f
Marcel Riera <mr.marcel.riera@gmail.com>
2019-01-30 10:06:41 -0500

    Added problem 13

--- Exploring Problem Set 1/Exploring Problem Set 1/Program.fs
+++ Exploring Problem Set 1/Exploring Problem Set 1/Program.fs
@@ -12,24 +12,34 @@ let main argv =
     printfn "B = %A" B
     printfn  "((List.map List.head) A) @ B = %A\n" (((List.map List.head) A) @ B)
 
+
     // Test for problem 8
+    let A = [1;2;3]
+    let B = [7;8]
     let rec foo = function
         | (xs, []) -> xs
         | (xs, y::ys) -> foo (xs@[y], ys)
-    let A = [1;2;3]
-    let B = [7;8]
-
+    
     printfn "Problem 8:"
     printfn "foo %A %A = %A\n" A B (foo (A,B))
 
+
     // Test rec format
     let rec fib = function
-        | n when n = 0 -> 1
+        | n when n = 0 -> 0
         | n when n = 1 -> 1
         | n -> fib (n-1) + fib (n-2) 
     let test = 4;
-
-    printfn "Testing rec formats"
+    
+    printfn "Testing rec format:"
     printfn "Fib(%i) = %A" test (fib(test))
+    
+    
+    // Test for problem 13
+    let F = fun xs -> List.map (+) xs
+    printfn "Problem 13:"
+    printfn "fun xs -> List.map (+) xs, where xs = %A, is %A" A (F A) 
+        // returns a list of functions, where each increments the input parameter by the amount of the element from which it was formed
+
     Console.ReadKey() |> ignore
     0
