b1edc829def69c1c5128d043125615de958a8802
Marcel Riera <mr.marcel.riera@gmail.com>
2019-02-26 18:39:39 -0500

    Finished - Created a list of Book records - Added a function to filter list in the book by genre

--- Testing Records/Testing Records/Program.fs
+++ Testing Records/Testing Records/Program.fs
@@ -1,4 +1,8 @@
-﻿
+﻿// Testing Records
+// Simple program to showcase the use of records.
+// It manipulates a list of Book records to show those with the specific 
+// genre Computer Science.
+
 open System
 
 type Book = { Id:int; Title:string; Author:string; Genre:string}
@@ -11,21 +15,31 @@ let b1 : Book = {
 }
 
 let b2 : Book = {
-    Id = 2322;
-    Title = "SQL database";
-    Author = "Henry B.";
-    Genre = "Computer Science"
-}
-
-let b3 : Book = {
     Id = 4425;
     Title = "Calculus II";
     Author = "Leonard T.";
     Genre = "Mathematics"
 }
 
+let b3 : Book = {
+    Id = 2322;
+    Title = "SQL database";
+    Author = "Henry B.";
+    Genre = "Computer Science"
+}
+
+let bookList = [b1; b2; b3]
+
+let filterByGenre genre bs = List.filter (fun book -> book.Genre = genre) bs
 
 [<EntryPoint>]
 let main argv =  
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "Testing Records\n"
+    printfn "Current records:"
+    printfn "%A\n" bookList
+
+    printfn "Computer Science books:"
+    printfn "%A\n" <| filterByGenre "Computer Science" bookList
+    
+    Console.ReadKey() |> ignore
+    0
