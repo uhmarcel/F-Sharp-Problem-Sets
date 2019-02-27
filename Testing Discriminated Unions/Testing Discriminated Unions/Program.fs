2c46d8ebd91a6cdb1638d32ca1ba97d3a5c363f8
Marcel Riera <mr.marcel.riera@gmail.com>
2019-02-26 19:03:27 -0500

    Project done Simple program to showcase unions - Added WeekDay union type - Added mySchedule function, taking a WeekDay and returning the availability for that day

---
+++ Testing Discriminated Unions/Testing Discriminated Unions/Program.fs
@@ -0,0 +1,31 @@
+﻿// Testing Discriminated Unions
+// Program showcasing a simple discriminated union with the type weekday.
+// It contains a function accepting a WeekDay union type which returns the 
+// schedule availability for that day.
+
+open System
+
+type WeekDay = 
+    | Monday
+    | Tuesday
+    | Wednesday
+    | Thursday
+    | Friday
+    | Saturday
+    | Sunday
+
+let mySchedule = function
+    | Monday | Wednesday -> "Busy"
+    | Tuesday | Thursday -> "Available from 12:00 pm to 5:00 pm"
+    | Friday -> "Available all workday"
+    | _ -> "No meeting on weekends"
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Testing Discriminated Unions"
+    printfn "My schedule on Thursday -> %A" <| mySchedule Thursday
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
