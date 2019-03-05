4cf96b4c8d9ec338b76c9c358c6458bb934c1833
Marcel Riera <mr.marcel.riera@gmail.com>
2019-03-04 22:56:56 -0500

    First commit - Started workbench program for problem set 2 - First attempt to P1-A

---
+++ Problem Set 2/Problem Set 2/Program.fs
@@ -0,0 +1,20 @@
+﻿
+open System
+
+
+// 1- Discriminated Union
+// a) Create a discriminated union for Coordinates that can be a Tuple, Threeple
+//    or Fourple that represent tuples of size two, three and four. 
+//    The type for the union should be polymorphic.
+
+type Coordinates =
+    | Tuple of unit * unit
+    | Threeple of unit * unit * unit
+    | Fourple of unit * unit * unit * unit
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Problem Set 2 Workbench"
+    Console.ReadKey() |> ignore
+    0
