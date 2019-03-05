4d71b0f3fb260c70eaca8c892d82190a82a2b6fe
Marcel Riera <mr.marcel.riera@gmail.com>
2019-03-05 00:51:20 -0500

    Added P1 a, b, c ,d - Currently have a ploblem, as (+) function is not generic expects type int, but some tuples are float and string

--- Problem Set 2/Problem Set 2/Program.fs
+++ Problem Set 2/Problem Set 2/Program.fs
@@ -2,15 +2,44 @@
 open System
 
 
-// 1- Discriminated Union
+// P1 - Discriminated Unions
+
 // a) Create a discriminated union for Coordinates that can be a Tuple, Threeple
 //    or Fourple that represent tuples of size two, three and four. 
 //    The type for the union should be polymorphic.
 
-type Coordinates =
-    | Tuple of unit * unit
-    | Threeple of unit * unit * unit
-    | Fourple of unit * unit * unit * unit
+type Coordinates<'a> =
+    | Tuple of 'a * 'a
+    | Threeple of 'a * 'a * 'a
+    | Fourple of 'a * 'a * 'a * 'a
+
+
+// b) Instantiate a Tuple of integers, a Threeple of floats and a Fourple of strings.
+
+let A = Tuple (2, 6)
+let B = Threeple (1.3, 4.2, 6.3)
+let C = Fourple ("and", "or", "not", "xor")
+
+
+// c) Create a function that has a parameter of a binary function and Coordinate. 
+//    Apply the function to the Coordinate like List.reduce.
+
+let rec coordinateReduce f = function
+    | Tuple (r,s) -> f r s
+    | Threeple (r,s,t) -> Tuple (f r s, t) |> coordinateReduce f
+    | Fourple (r,s,t,v) -> Threeple (f r s, t, v) |> coordinateReduce f
+
+
+// d) Call the function with (+) for each of the Coordinates in part (b).
+
+let inline (++) (x:'a) (y:'a) = x + y 
+
+let P1D_Result = [
+    coordinateReduce (+) A;
+    coordinateReduce (+) B;
+    coordinateReduce (+) C
+    ]
+
 
 
 [<EntryPoint>]
@@ -18,3 +47,7 @@ let main argv =
     printfn "Problem Set 2 Workbench"
     Console.ReadKey() |> ignore
     0
+    
+
+// Call the function with (+) for each of the Coordinates in part (b).
+// Call the function with (-) for the numeric Coordinates in part (b). Be sure that your function implements the normal associativity for (-).
