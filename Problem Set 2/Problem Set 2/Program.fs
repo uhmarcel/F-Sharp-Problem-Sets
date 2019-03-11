94e9930223db540fab9f365bb38bd3bef784f7da
Marcel Riera <mr.marcel.riera@gmail.com>
2019-03-11 00:14:46 -0400

    Problem 6 done - Added matrixProduct function. It required an aux function inside for recursion. It maps innerProduct for each row in the given matrix

--- Problem Set 2/Problem Set 2/Program.fs
+++ Problem Set 2/Problem Set 2/Program.fs
@@ -29,81 +29,36 @@ let rec coordinateReduce f = function
     | Fourple (r,s,t,v) -> Threeple (f r s, t, v) |> coordinateReduce f
 
 
-let ProblemOne =  
-
-    printfn "Problem 1\n"
-    printfn "Tuple A = %A" A
-    printfn "Threeple B = %A" B
-    printfn "Fourple C = %A\n" C
-    
-    // d) Call the function with (+) for each of the Coordinates in part (b).
-    printfn "coordinateReduce (+) A = %A" <|coordinateReduce (+) A
-    printfn "coordinateReduce (+) B = %A" <|coordinateReduce (+) B
-    printfn "coordinateReduce (+) C = %A" <|coordinateReduce (+) C
-
-    // e) Call the function with (-) for the numeric Coordinates in part (b). 
-    printfn "coordinateReduce (-) A = %A" <| coordinateReduce (-) A
-    printfn "coordinateReduce (-) B = %A" <| coordinateReduce (-) B
-    printfn "\n"
-    0
-
-
 // P2 - Creating a Syntax Parser
 
 type TERMINAL = IF|THEN|ELSE|BEGIN|END|PRINT|SEMICOLON|ID|EOF|ADD|SUB|MUL|LPAREN|RPAREN
 
-let ProblemTwo =
+let eat (T: TERMINAL) = function
+    | [] -> failwith "Incompleted syntax, program ended early"
+    | t::ts when t = T -> ts
+    | t::_ -> failwithf "Expected %A, but %A was found instead" T t
 
-    let eat (T: TERMINAL) = function
-        | [] -> failwith "Incompleted syntax, program ended early"
-        | t::ts when t = T -> ts
-        | t::_ -> failwithf "Expected %A, but %A was found instead" T t
-
-    let rec S = function 
-        | [] -> failwith "Incompleted syntax, program ended early"
-        | IF::ts -> ts |> eat ID |> eat THEN |> S |> eat ELSE |> S
-        | BEGIN::ts -> ts |> S |> L 
-        | PRINT::ts -> ts |> eat ID
-        | t::_ -> failwithf "Expected IF, BEGIN or PRINT. Found %A instead" t
+let rec S = function 
+    | [] -> failwith "Incompleted syntax, program ended early"
+    | IF::ts -> ts |> eat ID |> eat THEN |> S |> eat ELSE |> S
+    | BEGIN::ts -> ts |> S |> L 
+    | PRINT::ts -> ts |> eat ID
+    | t::_ -> failwithf "Expected IF, BEGIN or PRINT. Found %A instead" t
 
     
-    and L = function
-        | [] -> failwith "Incompleted syntax, program ended early"
-        | END::ts -> ts 
-        | SEMICOLON::ts -> ts |> S |> L
-        | t::_ ->  failwithf "Expected END or SEMICOLON. Found %A instead" t
+and L = function
+    | [] -> failwith "Incompleted syntax, program ended early"
+    | END::ts -> ts 
+    | SEMICOLON::ts -> ts |> S |> L
+    | t::_ ->  failwithf "Expected END or SEMICOLON. Found %A instead" t
 
-    let parseProgram p =
-        let parsed = p |> S       
-        match parsed with
-            | [] -> failwith "Missing EOF or terminated early"
-            | EOF::[] -> printfn "Program Accepted"
-            | EOF::_ -> failwith "EOF not at the end of the program"
-            | t::_ -> failwithf "Expected EOF, found %A instead" t
-
-    
-    let Program_A = [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;END;ELSE;PRINT;ID;EOF]
-    let Program_B = [IF;ID;THEN;IF;ID;THEN;PRINT;ID;ELSE;PRINT;ID;ELSE;BEGIN;PRINT;ID;END;EOF]
-    let Program_C = [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;SEMICOLON;END;ELSE;PRINT;ID;EOF]
-
-    printfn "Problem 2\n"
-
-    printfn "Program A: %A" Program_A
-    printfn "Program B: %A" Program_B
-    printfn "Program C: %A\n" Program_C
-
-    
-    printf "Program A -> " 
-    try parseProgram Program_A with | Failure(e) -> printfn "Syntax error: %s" e 
-    
-    printf "Program B -> " 
-    try parseProgram Program_B with | Failure(e) -> printfn "Syntax error: %s" e 
-    
-    printf "Program C -> " 
-    try parseProgram Program_C with | Failure(e) -> printfn "Syntax error: %s" e 
-    
-    printfn "\n"
-    0
+let parseProgram p =
+    let parsed = p |> S       
+    match parsed with
+        | [] -> failwith "Missing EOF or terminated early"
+        | EOF::[] -> printfn "Program Accepted"
+        | EOF::_ -> failwith "EOF not at the end of the program"
+        | t::_ -> failwithf "Expected EOF, found %A instead" t
  
 
 // P3 - Implement a parser using...
@@ -121,11 +76,98 @@ let ProblemTwo =
 // P4 - Define an F# function curry f that converts an uncurried function to a 
 // curried function, and an F# function uncurry f that does the opposite conversion. 
 
-let ProblemFour =
+let curry f a b = f (a,b)
+let uncurry f (a,b) = f a b
 
-    let curry f a b = f (a,b)
-    let uncurry f (a,b) = f a b
+
+// P5 - Given vectors u = (u1, u2,..., un) and v = (v1, v2,..., vn), the inner product
+// of u and v is defined to be u1*v1 + u2*v2 + ... + u n*vn. Write a curried F# function
+// inner that takes two vectors represented as int list and returns their inner product.
     
+let rec inner us vs = 
+    match us, vs with
+        | [], [] -> 0I
+        | [], _ | _, [] -> failwith "Lists are not the same size"
+        | u::us, v::vs -> u*v + inner us vs
+     
+let inner_tail us vs =
+    let rec inner_aux xs ys acc = 
+        match xs, ys with
+            | [], [] -> acc
+            | [], _ | _, [] -> failwith "Lists are not the same size"
+            | x::xs, y::ys -> inner_aux xs ys (acc + x*y)
+    inner_aux us vs 0I
+        
+   
+    
+
+// P6 - Given an m-by-n matrix A and an n-by-p matrix B, the product of A and B is an
+// m-by-p matrix whose entry in position (i,j) is the inner product of row i of A with 
+// column j of B.
+    
+let rec transpose = function
+    | []::_ -> []
+    | xs -> List.map (fun x -> List.head x) xs :: transpose (List.map (fun x -> List.tail x) xs)
+
+let rec innerProduct xs ys = 
+    match xs, ys with 
+        | [], [] -> 0
+        | _, [] | [], _ -> failwith "Lists must have same size"
+        | x::xs, y::ys -> x * y + innerProduct xs ys 
+    
+let matrixProduct A B =
+    let rec product_aux ys = function
+        | [] -> []
+        | x::xs -> List.map (innerProduct x) ys :: product_aux ys xs    
+    let B = transpose B
+    product_aux B A
+    
+    
+// Testing - Output for the problems
+
+[<EntryPoint>]
+let main argv =
+    
+    printfn "Problem 1\n"
+
+    printfn "Tuple A = %A" A
+    printfn "Threeple B = %A" B
+    printfn "Fourple C = %A\n" C
+    
+    printfn "coordinateReduce (+) A = %A" <|coordinateReduce (+) A
+    printfn "coordinateReduce (+) B = %A" <|coordinateReduce (+) B
+    printfn "coordinateReduce (+) C = %A" <|coordinateReduce (+) C
+    
+    printfn "coordinateReduce (-) A = %A" <| coordinateReduce (-) A
+    printfn "coordinateReduce (-) B = %A" <| coordinateReduce (-) B
+    printfn "\n"
+
+//  -------------------
+
+    printfn "Problem 2\n"
+    
+    let Program_A = [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;END;ELSE;PRINT;ID;EOF]
+    let Program_B = [IF;ID;THEN;IF;ID;THEN;PRINT;ID;ELSE;PRINT;ID;ELSE;BEGIN;PRINT;ID;END;EOF]
+    let Program_C = [IF;ID;THEN;BEGIN;PRINT;ID;SEMICOLON;PRINT;ID;SEMICOLON;END;ELSE;PRINT;ID;EOF]
+
+    printfn "Program A: %A" Program_A
+    printfn "Program B: %A" Program_B
+    printfn "Program C: %A\n" Program_C
+
+    
+    printf "Program A -> " 
+    try parseProgram Program_A with | Failure(e) -> printfn "Syntax error: %s" e 
+    
+    printf "Program B -> " 
+    try parseProgram Program_B with | Failure(e) -> printfn "Syntax error: %s" e 
+    
+    printf "Program C -> " 
+    try parseProgram Program_C with | Failure(e) -> printfn "Syntax error: %s" e 
+    
+    printfn "\n"
+    
+//  -------------------
+
     printfn "Problem 4\n"
     printf "%A - " <| (+) 2 4                       // No syntax error, it is curried
     printf "%A - " <| uncurry (+) (2,3)             // No syntax error, it is uncurried
@@ -135,67 +177,31 @@ let ProblemFour =
     printfn "val uncurry : f:('a -> 'b -> 'c) -> a:'a * b:'b -> 'c"
 
     printfn "\n"
-    0
 
-
-// P5 - Given vectors u = (u1, u2,..., un) and v = (v1, v2,..., vn), the inner product
-// of u and v is defined to be u1*v1 + u2*v2 + ... + u n*vn. Write a curried F# function
-// inner that takes two vectors represented as int list and returns their inner product.
-
-let ProblemFive =
+//  -------------------
     
-    let rec inner us vs = 
-        match us, vs with
-            | [], [] -> 0I
-            | [], _ | _, [] -> failwith "Lists are not the same size"
-            | u::us, v::vs -> u*v + inner us vs
-     
-    let inner_tail us vs =
-        let rec inner_aux xs ys acc = 
-            match xs, ys with
-                | [], [] -> acc
-                | [], _ | _, [] -> failwith "Lists are not the same size"
-                | x::xs, y::ys -> inner_aux xs ys (acc + x*y)
-        inner_aux us vs 0I
-        
     printfn "Problem 5\n"
 
     printfn "Inner function result -> Stack overflow" // <| inner [1I..50000I][50001I..100000I] -> Stack overflow!
     printfn "Inner_tail function result -> %A" <| inner_tail [1I..50000I][50001I..100000I]
     
     printfn "\n"
-    0
+
+//  -------------------
+
+    printfn "Problem 6\n"
     
+    let Matrix_A = [[1;2;3];[4;5;6]]
+    let Matrix_B = [[0;1];[3;2];[1;2]]
+        
+    printfn "Matrix A: %A" Matrix_A
+    printfn "Matrix B: %A \n" Matrix_B
+    printfn "Matrix A * Matrix B = %A \n" <| matrixProduct Matrix_A Matrix_B    
 
-// P6 - Given an m-by-n matrix A and an n-by-p matrix B, the product of A and B is an
-// m-by-p matrix whose entry in position (i,j) is the inner product of row i of A with 
-// column j of B.
+    printfn "\n"
 
-let problem 6 = 
-    
-    let rec transpose = function
-        | [] -> []
-        | xs     -> List.map (fun x -> List.head x) xs :: transpose (List.map (fun x -> List.tail x) xs)
+//  -------------------
 
-    let rec innerProduct xs ys = 
-        match xs, ys with 
-            | [], [] -> 0
-            | _, [] | [], _ -> failwith "Lists must have same size"
-            | x::xs, y::ys -> x * y + innerProduct xs ys 
-    
-
-    0
-
-
-[<EntryPoint>]
-let main argv =
-    
-    ProblemOne |> ignore
-    ProblemTwo |> ignore
-    //ProblemThree |> ignore
-    ProblemFour |> ignore
-    ProblemFive |> ignore
-    
     Console.ReadKey() |> ignore
     0
     
