d9a7d972752c7c0ad3ad57b7f4095da96148aa72
Marcel Riera <mr.marcel.riera@gmail.com>
2019-03-12 17:46:45 -0400

    Added function - Added insert function - Added test cases

--- Binary Search Tree/Binary Search Tree/Program.fs
+++ Binary Search Tree/Binary Search Tree/Program.fs
@@ -21,6 +21,11 @@ let rec find e = function
                         elif e < n then find e t1
                         else find e t2
 
+let rec insert e = function
+    | Lf -> Br (e, Lf, Lf)
+    | Br (n, t1, t2) -> if e < n then Br (n, insert e t1, t2)
+                        else Br (n, t1, insert e t2)
+
 
 [<EntryPoint>]
 let main argv =
@@ -28,14 +33,22 @@ let main argv =
     printfn "Binary Search Trees \n"
 
     let A = Br(9, Br(7, Br(2, Lf, Br(3, Lf, Lf)), Br(8, Lf, Lf)), Br(12, Br(11, Lf, Lf), Br(16, Lf, Lf)))
-    
-    printfn "Tree A:"
-    printfn "%A \n" A
+    let B = Br(6, Lf, Lf)
+
+    printfn "Tree A: \n %A \n" A
+    printfn "Tree B: \n %A \n" B
 
     printfn "minimum A = %A" <| minimum A
-    printfn "maximum A = %A" <| maximum A
+    printfn "maximum A = %A\n" <| maximum A
+
     printfn "find 11 A = %A" <| find 11 A
-    printfn "find 6 A = %A" <| find 6 A
+    printfn "find 11 B = %A\n" <| find 11 B
+
+    printfn "insert 8 B = %A" <| insert 8 B 
+    printfn "insert 2 B = %A" <| insert 2 B
+    printfn "B |> insert 7 |> insert 12 = %A" <| (B |> insert 7 |> insert 12)
+
+
 
 
 
