205396d4efcd191a38da459f500cb2e22028bde5
Marcel Riera <mr.marcel.riera@gmail.com>
2019-04-11 23:03:25 -0400

    Started Problem Set 3 Project - Added LinkedList type - Added toLinkedList function taking a list

---
+++ Problem Set 3/Problem Set 3/Program.fs
@@ -0,0 +1,22 @@
+﻿
+open System
+
+
+// P1 - Building a simple tree.
+
+// Create a discriminated union that can represent a linked list of integers.
+// Write a function that converts a list into a linked list of nodes.
+
+type 'a LinkedList =
+    | Node of 'a * 'a LinkedList
+    | Null
+
+let rec toLinkedList = function
+    | [] -> Null
+    | x::xs -> Node (x, toLinkedList xs)
+    
+
+[<EntryPoint>]
+let main argv =
+    Console.ReadKey() |> ignore
+    0
