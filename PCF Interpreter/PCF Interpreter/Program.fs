40a36e8790ee71c2e8692ea5310046f830d5e934
Marcel Riera <mr.marcel.riera@gmail.com>
2019-04-18 19:32:27 -0400

    PCF Interpreter - Added return case to interpreter - Added first two test functions succ 0 & succ 1

--- PCF Interpreter/PCF Interpreter/Program.fs
+++ PCF Interpreter/PCF Interpreter/Program.fs
@@ -7,12 +7,13 @@ open System
 // PCF interpreter
 
 let rec interp = function
-| APP (e1, e2) ->
-    match (interp e1, interp e2) with
-    | (ERROR s, _)  -> ERROR s        // ERRORs are propagated
-    | (_, ERROR s)  -> ERROR s
-    | (SUCC, NUM n) -> NUM (n+1)      // Rule (6)
-    | (SUCC, v)     -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
+    | APP (e1, e2) ->
+        match (interp e1, interp e2) with
+        | (ERROR s, _)  -> ERROR s        // ERRORs are propagated
+        | (_, ERROR s)  -> ERROR s
+        | (SUCC, NUM n) -> NUM (n+1)      // Rule (6)
+        | (SUCC, v)     -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
+    | n -> n
 
 
 
@@ -23,9 +24,19 @@ let interpstr sourcecode = sourcecode |> parsestr |> interp
 
 
 // Test Workspace
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Test" 
+
+    printfn "Testing PCF Interpreter \n"
+    
+    let displayInterpstr s = printfn "%s -> %A" s (interpstr s)
+    let displayInterpfile s = printfn "%s -> %A" s (interpfile s)
+
+    displayInterpstr "succ 0"
+    displayInterpstr "succ 1"
+
     Console.ReadKey() |> ignore
     0
 
