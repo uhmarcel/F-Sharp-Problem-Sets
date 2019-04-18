258f4e750c0a023b2da1dfbe90180feedea3095d
Marcel Riera <mr.marcel.riera@gmail.com>
2019-04-18 19:15:45 -0400

    PCF Interpreter - Added parser.fs - Added interpreter skeleton

---
+++ PCF Interpreter/PCF Interpreter/Program.fs
@@ -0,0 +1,31 @@
+﻿module Interp
+
+open Parser.Parse
+open System
+
+
+// PCF interpreter
+
+let rec interp = function
+| APP (e1, e2) ->
+    match (interp e1, interp e2) with
+    | (ERROR s, _)  -> ERROR s        // ERRORs are propagated
+    | (_, ERROR s)  -> ERROR s
+    | (SUCC, NUM n) -> NUM (n+1)      // Rule (6)
+    | (SUCC, v)     -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
+
+
+
+// Interpreter abbreviations
+
+let interpfile filename = filename |> parsefile |> interp
+let interpstr sourcecode = sourcecode |> parsestr |> interp
+
+
+// Test Workspace
+[<EntryPoint>]
+let main argv =
+    printfn "Test" 
+    Console.ReadKey() |> ignore
+    0
+
